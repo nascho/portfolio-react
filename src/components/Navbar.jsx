8ae770deae9603df64df94ccd299d1bce7d6aea6
Nas <n.choudhury1@live.co.uk>
2024-04-06 22:05:19 +0100

    included url links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,12 +45,12 @@ const Navbar = () => {
             <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                        <a href="/" className="flex justify-between items-center w-full text-gray-300">
+                        <a href="https://www.linkedin.com/in/naschoudhury" target="blank" className="flex justify-between items-center w-full text-gray-300">
                             LinkedIn <FaLinkedin size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                        <a href="/" className="flex justify-between items-center w-full text-gray-300">
+                        <a href="https://github.com/nascho" target="blank" className="flex justify-between items-center w-full text-gray-300">
                             GitHub <FaGithub size={30} />
                         </a>
                     </li>
