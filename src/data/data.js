33ec6541ec0e5ac8d052393b4ff2a42c0a0a1114
Nas <n.choudhury1@live.co.uk>
2024-04-06 22:07:08 +0100

    created mock data for proejcts card section

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data = [ 
+    {
+        id:1,
+        name:"React JS Application - 1",
+        image:WorkIm,
+        github:"https://github.com/nascho/portfolio-react",
+        live:"https://github.com/nascho/portfolio-react",
+    },
+    {
+        id:2,
+        name:"React JS Application - 2",
+        image:RealEst,
+        github:"",
+        live:"https://github.com/nascho/portfolio-react",
+    },
+    {
+        id:3,
+        name:"React JS Application - 3",
+        image:WorkIm,
+        github:"https://github.com/nascho/portfolio-react",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application - 4",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+]
+
