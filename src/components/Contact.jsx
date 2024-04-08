f7aa933d602df3f112b6c5a5da01c1c387f0b7e6
Nas <n.choudhury1@live.co.uk>
2024-04-08 13:01:50 +0100

    changed the form size to match sizing of site

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/pbnvxydb" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/pbnvxydb" className='flex flex-col max-w-[972px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
             </div>
