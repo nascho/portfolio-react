e773a77444826b21c89583b5e45319629a8710b1
Nas <n.choudhury1@live.co.uk>
2024-04-08 11:48:30 +0100

    updated name property

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div name='name' className='w-full h-screen bg-[#0a192f]'>
-        
-        {/** container */}
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
