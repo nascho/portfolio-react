9fe2da48595a6aeb402ec2b6c73ccb5bfe81c58e
Nas <n.choudhury1@live.co.uk>
2024-04-14 19:54:38 +0100

    changed padding on about container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div name="about" className='w-full min-h-screen bg-[#0a192f] text-gray-300'>
-        <div className="flex flex-col justify-center items-center p-4 w-full h-full">
+        <div className="flex flex-col justify-center items-center py-4 w-full h-full">
             <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
                 <div className='pb-8 pl-4'>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
@@ -11,10 +11,10 @@ const About = () => {
                 <div></div>
             </div>
             <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-                <div className='text-4xl font-bold'>
+                <div className='text-4xl font-bold text-[#8892b0]'>
                     <p>Hi, I'm Nas, nice to meet you. Please take a look around.</p>
                 </div>
-                <div className='text-justify'>
+                <div className='text-justify text-[#8892b0]'>
                     <p> 
                         As an enthusiastic and dedicated software developer in both Python / WagtailCMS and JavaScript / NodeJS, I bring a positive attitude, a desire for learning, and a strong sense of discipline to every project. I am seeking opportunities to continuously expand my skill set while embracing diverse perspectives. I am passionate about contributing my expertise to a company that prioritises and advocates for a culture of diversity and inclusivity.
                     </p>  
