bb6ef20e7bf3e574a7946027628c8d8e76ef8e5e
Nas <n.choudhury1@live.co.uk>
2024-04-19 19:22:10 +0100

    created object for calculator app

--- src/data/data.js
+++ src/data/data.js
@@ -3,43 +3,52 @@ import PHAA from '../assets/projects/phaa.png';
 import Discord from '../assets/projects/discord.png';
 import Blog from '../assets/projects/blog.png';
 import Todo from '../assets/projects/todo.png';
+import Calculator from '../assets/projects/calculator.png';
 
 
 export const data = [ 
     {
         id:1,
-        name:"BootStrap, Python & Wagtail CMS for Person Centred Universe website",
+        name:"BootStrap, Python & Wagtail CMS - Person Centred Universe website",
         image:PCU,
         github:"",
         site:"https://www.personcentreduniverse.com",
     },
     {
         id:2,
-        name:"HTML, CSS & JavaScript, Adoption Agency website for Founders & Coders",
+        name:"HTML, CSS, ReactJS, JavaScript & NodeJS - Calculator App",
+        image:Calculator,
+        github:"https://github.com/nascho/react-calculator.git",
+        site:"http://44.202.41.185",
+    },
+    {
+        id:3,
+        name:"HTML, CSS & JavaScript, Adoption Agency website - Founders & Coders",
         image:PHAA,
         github:"https://github.com/fac30/nas-isaac-bahja-agency.git",
         site:"",
     },
     {
-        id:3,
-        name:"JavaScript & Jest Test Framework, Discord OpenAI Chatbot for Founders & Coders",
+        id:4,
+        name:"JavaScript & Jest Test Framework, Discord OpenAI Chatbot - Founders & Coders",
         image:Discord,
         github:"https://github.com/fac30/bahja-isaac-nas-discord-chatbot.git",
         site:"",
     },
     {
-        id:4,
-        name:"BootStrap, JavaScript, EJS, NodeJS, Mongoose, MongoDB & OpenAI Blog site for Founders & Coders",
+        id:5,
+        name:"BootStrap, JavaScript, EJS, NodeJS, Mongoose, MongoDB & OpenAI Blog site - Founders & Coders",
         image:Blog,
         github:"https://github.com/fac30/ali-nas-travel-blog.git",
         site:"",
     },
     {
-        id:4,
-        name:"HTML, CSS & JavaScript Todo Application",
+        id:6,
+        name:"HTML, CSS & JavaScript - Todo App",
         image:Todo,
         github:"https://github.com/nascho/todo_list.git",
         site:"",
     },
+
 
 ]
